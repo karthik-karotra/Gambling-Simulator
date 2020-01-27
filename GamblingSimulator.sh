69a775ebddfef7f8d027f2ca54bdf73707d630ff
karthik-karotra <karthikpatel54@gmail.com>
2020-01-27 11:35:06 +0530

    ADDED:Resigning condition using While statement

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -3,15 +3,25 @@ echo "Welcome to Gambling Simulation"
 
 #CONSTANTS
 BET=1
+MINIMUMLIMIT=50
+MAXIMUMLIMIT=150
 
 #VARIABLES
 stake=100
 
-if [ $((RANDOM%2)) -eq 1 ]
+while [[ $stake -ne $MINIMUMLIMIT && $stake -ne $MAXIMUMLIMIT ]]
+do
+	if [ $((RANDOM%2)) -eq 1 ]
+	then
+		stake=$(($stake+$BET))
+	else
+		stake=$(($stake-$BET))
+	fi
+done
+
+if [ $stake -eq $MINIMUMLIMIT ]
 then
-	echo "Won Bet"
-	stake=$(($stake+$BET))
+	echo "You have lost 50% of your daily stake.Its better you resign for the day"
 else
-	echo "Lose Bet"
-	stake=$(($stake-$BET))
+	echo "Bravo!!! You reached your goal. "
 fi
