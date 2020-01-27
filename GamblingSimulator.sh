045c15efb687058d4dd065bb4f5f8dcb0d25a239
karthik-karotra <karthikpatel54@gmail.com>
2020-01-27 16:03:20 +0530

    ADDED: Added variables for calculating no of days won or lost

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -4,14 +4,17 @@ echo "Welcome to Gambling Simulation"
 #CONSTANTS
 BET=1
 MINIMUM_LIMIT=50
-MAXIMUM_LIMIT=150
+MAXIMUM_LIMIT=150	
+WON_COUNT=0
+LOST_COUNT=0
+NO_OF_DAYS=20
 
 #VARIABLES
 stake=100
 winningAmount=0
 looseingAmount=0
 
-for((i=1;i<=20;i++))
+for((i=1;i<=$NO_OF_DAYS;i++))
 do
 	while [[ $stake -ne $MINIMUM_LIMIT && $stake -ne $MAXIMUM_LIMIT ]]
 	do
@@ -26,9 +29,11 @@ do
 	then
 		loosingAmount=$(($loosingAmount + 50))
 		stake=100
+		(( LOST_COUNT++ ))
 	else
 		winningAmount=$(($winningAmount+50))
 		stake=100
+		(( WON_COUNT++ ))
 	fi
 done
 if [ $winningAmount -gt $loosingAmount ]
@@ -40,3 +45,8 @@ then
 else
 	echo "You neither lose nor win"
 fi
+
+echo "No of days Gambler won is $WON_COUNT"
+echo "No of days Gambler lost is $LOST_COUNT"
+echo "Total Amount won is $(($WON_COUNT*50))"
+echo "Total amount lost is $(($LOST_COUNT*50))"
