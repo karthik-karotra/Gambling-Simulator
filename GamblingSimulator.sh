f8d7fce417bc58bd2b447d516c224ca4b665e48a
karthik-karotra <karthikpatel54@gmail.com>
2020-01-28 13:32:11 +0530

    ADDED:Checked Luckiest and Unluckiest day

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -1,18 +1,20 @@
-#!/bin/bash -x
+#!/bin/bash
+declare -A checkLuckiestUnluckiest
 echo "Welcome to Gambling Simulation"
 
 #CONSTANTS
 BET=1
 MINIMUM_LIMIT=50
 MAXIMUM_LIMIT=150	
-WON_COUNT=0
-LOST_COUNT=0
 NO_OF_DAYS=20
 
 #VARIABLES
 stake=100
 winningAmount=0
+wonCount=0
+lostCount=0
 looseingAmount=0
+currentAmount=0
 
 for((i=1;i<=$NO_OF_DAYS;i++))
 do
@@ -29,12 +31,15 @@ do
 	then
 		loosingAmount=$(($loosingAmount + 50))
 		stake=100
-		(( LOST_COUNT++ ))
+		(( lostCount++ ))
+		currentAmount=$(($currentAmount-50))
 	else
 		winningAmount=$(($winningAmount+50))
 		stake=100
-		(( WON_COUNT++ ))
+		(( wonCount++ ))
+		currentAmount=$(($currentAmount+50))
 	fi
+	checkLuckiestUnluckiest[$i]=$currentAmount
 done
 if [ $winningAmount -gt $loosingAmount ]
 then
@@ -46,7 +51,21 @@ else
 	echo "You neither lose nor win"
 fi
 
-echo "No of days Gambler won is $WON_COUNT"
-echo "No of days Gambler lost is $LOST_COUNT"
-echo "Total Amount won is $(($WON_COUNT*50))"
-echo "Total amount lost is $(($LOST_COUNT*50))"
+echo "No of days Gambler won is $wonCount"
+echo "No of days Gambler lost is $lostCount"
+echo "Total Amount won is $winningAmount"
+echo "Total amount lost is $loosingAmount"
+echo ${!checkLuckiestUnluckiest[@]}
+echo ${checkLuckiestUnluckiest[@]}
+
+function checkGamblerLuck() {
+	for i in ${!checkLuckiestUnluckiest[@]}
+	do
+		echo "$i ${checkLuckiestUnluckiest[$i]}"
+	done | sort -k2 $1 | head -1
+}
+
+echo "Luckiest Day with amount is"
+checkGamblerLuck -rn
+echo "Unluckiest Day with amount is"
+checkGamblerLuck -n
